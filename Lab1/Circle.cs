dd1bb70059628f035d0bfa7041537a187037ee18
Kizatov_Arman <kizatov.armashka.05@gmail.com>
2018-01-10 11:37:49 +0600

    KBTU

---
+++ Lab1/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab1
+
+{
+    public class Circle
+    {
+        public double radius;
+        public double area;
+        public double perimetre;
+
+
+        public Circle() // empty constructor;
+        {
+            radius = 15;
+        }
+
+        public Circle(double radius)
+        { // constructor with some intial data + calcilate S and P;
+            this.radius = radius;
+            findArea();
+            findPerimetre();
+        }
+
+        public void findArea() // functions to Find Area and Perimetre;
+        {
+            area = Math.PI * radius * radius;
+        }
+
+        public void findPerimetre()
+        {
+            perimetre = 2 * Math.PI * radius;
+        }
+
+        public override string ToString(){
+            return "Your radius = " + radius + "\nArea = " + area + " Peraimetre = " + perimetre;
+        }
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine()) ;
+
+            Circle c1 = new Circle(r);
+
+            Console.WriteLine(c1);
+            Console.ReadKey();
+        }
+    }
+}
