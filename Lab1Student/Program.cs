f6b9edec98305655851b8c833faeeec4b27a40ad
Kizatov_Arman <kizatov.armashka.05@gmail.com>
2018-01-12 00:45:13 +0600

    KBTU

---
+++ Lab1Student/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab1Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double g1 = double.Parse(Console.ReadLine());
+            double g2 = double.Parse(Console.ReadLine());
+            double g3 = double.Parse(Console.ReadLine());
+            double g4 = double.Parse(Console.ReadLine());
+            Student S1 = new Student();
+            Console.WriteLine(S1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1Student/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lab1Student
+{
+    public class Student
+    {
+        public string name;
+        public string surname;
+        public double gpa1;
+        public double gpa2;
+        public double gpa3;
+        public double gpa4;
+        public double gpaaver;
+
+        public Student()
+        {
+            name = "Arman";
+            surname = "Kizatov";
+            gpa1 = 4.00;
+            gpa2 = 4.00;
+            gpa3 = 4.00;
+            gpa4 = 4.00;
+            FindAverGpa();
+        }
+
+        public Student(string name, string surname, double gpa1, double gpa2, double gpa3, double gpa4)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.gpa1 = gpa1;
+            this.gpa2 = gpa2;
+            this.gpa3 = gpa3;
+            this.gpa4 = gpa4;
+        }
+
+        public void FindAverGpa()
+        {
+            gpaaver = (gpa1 + gpa2 + gpa3 + gpa4) / 4;   
+        }
+
+        public override string ToString()
+        {
+            return name + " " + surname + "\nGPA = " + gpaaver;
+        }
+    }
+}
