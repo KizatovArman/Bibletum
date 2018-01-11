b62b8f5932039abf164b33a839113a11a31b9c03
Kizatov_Arman <kizatov.armashka.05@gmail.com>
2018-01-11 23:59:38 +0600

    KBTU

---
+++ Lab1rectangl/Lab1rectangl/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Lab1rectangl
+{
+    
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            
+            Rectangle R1 = new Rectangle();
+
+            Console.WriteLine(R1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1rectangl/Lab1rectangl/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lab1rectangl
+{
+    public class Rectangle
+    {
+        public double width;
+        public double lenght;
+        public double perimetre;
+        public double area;
+
+
+        public Rectangle()
+        {
+            width = 123;
+            lenght = 230;
+            FindArea();
+            FindPerimetre();
+        }
+
+        public Rectangle(double width, double lenght)
+        {
+            this.width = width;
+            this.lenght = lenght;
+
+        }
+
+        public void FindArea()
+        {
+            area = width * lenght;
+        }
+
+        public void FindPerimetre()
+        {
+            perimetre = 2 * (width + lenght);
+        }
+
+
+        public override string ToString()
+        {
+            return "Dimansions are " + width + " " + lenght + "\nArea = " + area + "\nPerimetre = " + perimetre;
+        }
+    }
+
+}
