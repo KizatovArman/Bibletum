5aea456f5ef67e3daa7dd6f49f8ab9d0b30b3b82
Kizatov_Arman <kizatov.armashka.05@gmail.com>
2018-01-14 21:14:11 +0600

    KBTU

--- Lab1Task1/Lab1Task1/Program.cs
+++ Lab1Task1/Lab1Task1/Program.cs
@@ -4,50 +4,17 @@
 
 namespace Lab1Task1
 {
+    
     class Program
     {
-        static void Main(string[] args)
-        {
-            /*
-            string s = Console.ReadLine();
-            int n = int.Parse(s);
-            for (int i = 0; i < n; i++)
-            {
-                string s1 = Console.ReadLine();
-                int x = int.Parse(s1); 
-                if (IsPrime(x)){
-                    Console.WriteLine(x);
-                }
-            }
-            */
-
-            int n;
-            n = Convert.ToInt16(Console.ReadLine());
-            // массив A с n-элементами 
-            double[] A = new double[n];
-            int i = 0;
-            while (i < n)
-            {
-                /* Вводим элементы массива с клавиатуры 
-                 * и заполняем ими массив */
-                A[i] = double.Parse(Console.ReadLine());
-                Console.WriteLine();
-                i++;
-            }
-            // Вывод всех прайм чисел
-            for (i = 0; i < n; i++)
-            {
-                if (IsPrime(A[i]))
-                Console.WriteLine(A[i] + " ");
-                Console.ReadKey();
-            }
-        }
-        //метод определябщий является число простым или нет
-        public static bool IsPrime(double N)
+        public static bool IsPrime(int N)
         {
             bool res = false;
 
-            for (double i = 2; i < (N / 2); i++)
+            if (N == 2)
+                res = true;
+            
+            for (int i = 2; i <= (N / 2); i++)
             {
                 if (N % i == 0)
                 {
@@ -55,13 +22,27 @@ namespace Lab1Task1
                     break;
                 }
                 else
-                {
                     res = true;
-                }
             }
             return res;
         }
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 2, 3, 5, 7, 14, 17 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (IsPrime(array[i]))
+                {
+                    Console.WriteLine(array[i]);
+                    Console.ReadKey();
+                }
+            }
+
+
+        }
     }
-}
+}       
+
+
 
         
