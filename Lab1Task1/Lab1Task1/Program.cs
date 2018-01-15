26643f6d5b228988ea1a6358382f0a3956369916
Kizatov_Arman <kizatov.armashka.05@gmail.com>
2018-01-15 10:37:04 +0600

    IT IS DONE

--- Lab1Task1/Lab1Task1/Program.cs
+++ Lab1Task1/Lab1Task1/Program.cs
@@ -1,18 +1,12 @@
 ﻿using System;
 
-//почему не получается??? помогите
-
 namespace Lab1Task1
 {
-    
     class Program
     {
-        public static bool IsPrime(int N)
+        public static bool IsPrime(int N)// method to determine primeness of numbers;
         {
-            bool res = false;
-
-            if (N == 2)
-                res = true;
+            bool res = true;
             
             for (int i = 2; i <= (N / 2); i++)
             {
@@ -21,13 +15,12 @@ namespace Lab1Task1
                     res = false;
                     break;
                 }
-                else
-                    res = true;
             }
             return res;
         }
         static void Main(string[] args)
         {
+            /*
             int[] array = new int[] { 2, 3, 5, 7, 14, 17 };
             for (int i = 0; i < array.Length; i++)
             {
@@ -37,8 +30,18 @@ namespace Lab1Task1
                     Console.ReadKey();
                 }
             }
-
-
+            I tried to find prime with "ready array";
+            */
+            string str = Console.ReadLine();//we gather our input
+            args = str.Split(' ');//split our input string and obtain something like this {"1","2"...}
+            foreach(string S in args)//cycle to look through our variables(циферки)
+            {
+                int ch1 = int.Parse(S);
+                if (IsPrime(ch1)){//check for primeness
+                    Console.WriteLine(ch1);//and print the answer
+                }
+            }
+            Console.ReadKey();// wait until user press any button
         }
     }
 }       
