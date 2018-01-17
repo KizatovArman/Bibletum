d1de12a56f945839365cecb24d51f9193ffdcfa2
Kizatov_Arman <kizatov.armashka.05@gmail.com>
2018-01-17 09:46:10 +0600

    LAB2

---
+++ W2Lab2/W2Lab2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace W2Lab2
+{
+    class Program
+    {
+        public static int mini = 10000;
+        public static int maxi = 0;
+
+        static void Main(string[] args)
+        {
+
+            string str = System.IO.File.ReadAllText(@"/Users/arman/Documents/Bibletum/W2Lab2/W2Lab2/input.txt");
+
+            args = str.Split(" ");
+
+            foreach(string s in args)
+            {
+                int ch1 = int.Parse(s);
+                if (ch1>maxi)
+                {
+                    maxi = ch1;
+                }
+
+                if (ch1 < mini)
+                {
+                    mini = ch1;
+                }
+
+            }
+            Console.WriteLine(maxi + "\n" + mini);
+            Console.ReadKey();
+        }
+    }
+}
