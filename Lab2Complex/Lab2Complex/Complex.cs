ac466b3076fede6a98a0c3405d8fd9cec4a24e2c
Kizatov_Arman <kizatov.armashka.05@gmail.com>
2018-01-17 10:49:18 +0600

    Lab2

---
+++ Lab2Complex/Lab2Complex/Complex.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Lab2Complex
+
+{
+    public class Complex
+
+    {
+        public int x, y;
+
+        public Complex(){}
+
+        public Complex(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        public static Complex operator *(Complex com1, Complex com2)
+        {
+            Complex A = new Complex(com1.x * com2.x, com1.y * com2.y);
+            A.Simplification();
+            return A;
+        }
+
+        public static Complex operator /(Complex com3, Complex com4)
+        {
+            Complex B = new Complex(com3.x * com4.y, com3.y * com4.x);
+            B.Simplification();
+            return B;
+        }
+
+        public static Complex operator +(Complex com5, Complex com6)
+        {
+            Complex C = new Complex(com5.x * com6.y + com5.y * com6.x, com5.y * com6.y);
+            C.Simplification();
+            return C;
+        }
+
+        public static Complex operator -(Complex com7, Complex com8)
+        {
+            Complex D = new Complex(com7.x * com8.y - com7.y * com8.x, com7.y * com8.y);
+            D.Simplification();
+            return D;
+        }
+
+        public override string ToString()
+        {
+            return x + "/" + y;
+        }
+
+        public void Simplification()
+        {
+            int _x = this.x;
+            int _y = this.y;
+            while (_x > 0 && _y > 0)
+            {
+                if (_x > _y)
+                    _x = _x % _y;
+                else
+                    _y = _y % _x;
+            }
+            int gcd = _x + _y;
+            x /= gcd;
+            y /= gcd;
+        }
+    }
+}
---
+++ Lab2Complex/Lab2Complex/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab2Complex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] token = s.Split();
+            string[] s1 = token[0].Split('/'); 
+            string[] s2 = token[1].Split('/');
+            Complex a = new Complex(int.Parse(s1[0]), int.Parse(s1[1]));
+            Complex b = new Complex(int.Parse(s2[0]), int.Parse(s2[1]));
+            Complex c = a * b;
+            Complex d = a / b;
+            Complex e = a + b;
+            Complex f = a - b;
+            Console.WriteLine(c + "\n" + d+ "\n" + e + "\n" + f);
+            Console.ReadKey();
+
+        }
+    }
+}
