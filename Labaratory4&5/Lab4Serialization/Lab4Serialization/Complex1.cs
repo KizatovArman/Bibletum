342673135e7ee7c7f4aa293359051fce7c7b31b4
Kizatov_Arman <kizatov.armashka.05@gmail.com>
2018-02-06 20:09:58 +0600

    xml serialization

---
+++ Labaratory4&5/Lab4Serialization/Lab4Serialization/Complex1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+namespace Lab4Serialization
+{
+    [Serializable]
+    public class Complex1
+    {
+        
+        public int x, y;
+        public Complex1(){}
+
+
+        public Complex1(int _x,int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+       /* public static  Complex1 operator *(Complex1 com1, Complex1 com2)
+        {
+             Complex1 A = new Complex1(com1.x * com2.x, com1.y * com2.y);
+            A.Simplizer();
+            return A;
+
+        }
+
+        public static Complex1 operator /(Complex1 com3, Complex1 com4)
+        {
+            Complex1 B = new Complex1(com3.x * com4.y, com3.y * com4.x);
+            B.Simplizer();
+            return B;
+        }
+        */
+        // and other "перегружение операторов"
+
+        public override string ToString()
+        {
+            return x + "/" + y;
+        }
+        /*
+        public void Simplizer()
+        {
+            int _x = x;
+            int _y = y;
+            int gcd;
+
+            while (_x>0 && y>0)
+            {
+                if (_x > _y)
+                    _x = _x % _y;
+                else
+                    _y = _x % _y;
+            }
+            gcd = _x + _y;
+
+            x /= gcd;
+            y /= gcd;
+            */
+        }
+    }
+
--- Labaratory4&5/Lab4Serialization/Lab4Serialization/Program.cs
+++ Labaratory4&5/Lab4Serialization/Lab4Serialization/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace Lab4Serialization
 {
     class Program
     {
+        static void FunctionToSer()
+        {
+            FileStream fs = new FileStream(@"data.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);// не знаю почему но на маке не хочет создавать data.xml
+
+            XmlSerializer xs = new XmlSerializer(typeof(Complex1));
+
+            Complex1 c1 = new Complex1();
+            try
+            {
+                xs.Serialize(fs, c1);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                fs.Close();
+            }
+            Console.WriteLine("done");
+        }
+
+        static void FunctionToDeSer()
+        {
+
+            FileStream sf = new FileStream(@"data.xml", FileMode.Open, FileAccess.Read);
+
+            XmlSerializer sx = new XmlSerializer(typeof(Complex1));
+
+            try
+            {
+                Complex1 c2 = sx.Deserialize(sf) as Complex1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);// сообщить об ошибке
+            }
+            finally
+            {
+                sf.Close();
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           //
+            FunctionToDeSer();
+            Console.ReadKey();
         }
     }
 }
