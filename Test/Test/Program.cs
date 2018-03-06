bcba40c75245531655836256b8e0645425ad8f8f
Kizatov_Arman <kizatov.armashka.05@gmail.com>
2018-03-06 20:17:15 +0600

    123

---
+++ Test/Test/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace Test
+{
+    class Program
+    {
+
+        public static bool IsPrime(int x)
+        {
+            bool res = true;
+            for (int i = 2; i <= x / 2; i++)
+            {
+                if (x%i==0)
+                {
+                    res=false;
+                    break;
+                }
+            }
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter(@"/Users/arman/Desktop/output.txt");
+            StreamReader sr = new StreamReader(@"/Users/arman/Desktop/input.txt");
+            string str = sr.ReadLine();
+            int ch1 = int.Parse(str);
+            StreamReader sr2 = new StreamReader(@"/Users/arman/Desktop/input.txt");
+            string st = sr.ReadToEnd();
+            string[] s = st.Split(" ");
+            for (int j = 0; j < ch1; j++)
+            {
+                int ch2 = int.Parse(s[j]);
+                if (IsPrime(ch2))
+                {
+                    Console.WriteLine(ch2.ToString());
+                    Console.ReadKey();
+                    sw.Write(ch2.ToString()+" "); 
+                }
+            }
+            sw.Close();
+
+        }
+    }
+}
